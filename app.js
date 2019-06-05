d735ee2b032c63aecb47a092d6a00e300ae623eb
Shayant Sital <shayant@gmail.com>
2019-06-04 22:19:54 -0300

    changed 'cast' endpoint to 'fullcast'

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const express = require("express");
-const scraper = require('./scraper');
+const scraper = require("./scraper");
 
+const app = express();
 
-const app = express()
-
-app.get('/', (req, res, next) => {
-    res.json({
-        message: 'Scraping'
-    })
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Scraping"
+  });
 });
 // /search/star wars
-app.get('/search/:title', (req, res, next) => {
-    scraper.searchMovies(req.params.title).then(movies => {
-        res.json(movies)
-    })
+app.get("/search/:title", (req, res, next) => {
+  scraper.searchMovies(req.params.title).then(movies => {
+    res.json(movies);
+  });
 });
-app.get('/movie/:movieId', (req, res, next) => {
-    scraper.getMovie(req.params.movieId).then(movieData => {
-        res.json(movieData)
-    })
+app.get("/movie/:movieId", (req, res, next) => {
+  scraper.getMovie(req.params.movieId).then(movieData => {
+    res.json(movieData);
+  });
 });
-app.get('/cast/:movieId', (req, res, next) => {
-    scraper.getMovieCast(req.params.movieId).then(castData => {
-        res.json(castData)
-    })
+app.get("/fullcast/:movieId", (req, res, next) => {
+  scraper.getMovieCast(req.params.movieId).then(castData => {
+    res.json(castData);
+  });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`)
-})
+  console.log(`Listening on port: ${port}`);
+});
